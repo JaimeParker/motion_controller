d7ba96c7d65635e26cbee8af5fe93a64ea071455
Zhaohong Liu <jaimefriedhelmzhao@gmail.com>
2023-07-20 20:41:04 +0800

    reconstruct offboard waypoint flight code and launch file

--- px4_mavros_controller/src/offb_node.cc
+++ px4_mavros_controller/src/offb_node.cc
@@ -3,15 +3,15 @@
  * @brief Offboard control example node, written with MAVROS version 0.19.x, PX4 Pro Flight
  * Stack and tested in Gazebo Classic SITL
  * reference: https://docs.px4.io/main/en/ros/mavros_offboard_cpp.html
+ * after revise, this exe tested successfully in real test using a 450 mm quad rotor drone
  */
-
-// add some information output
 
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
+#include <vector>
 
 // call back function
 mavros_msgs::State current_state;
@@ -24,11 +24,16 @@ void local_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
     current_pose = *msg;
 }
 
+inline bool reached(geometry_msgs::PoseStamped& pose,
+                    geometry_msgs::PoseStamped& target ){
+    return sqrt(pow(pose.pose.position.x - target.pose.position.x, 2)
+    + pow(pose.pose.position.y - target.pose.position.y, 2)
+    + pow(pose.pose.position.z - target.pose.position.z, 2)) < 0.2;
+}
+
 int main(int argc, char **argv)
 {
-    // init ros node
     ros::init(argc, argv, "offb_node");
-    // create node handler
     ros::NodeHandle nh;
 
     // define subscriber and publisher
@@ -44,6 +49,16 @@ int main(int argc, char **argv)
     ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
             ("mavros/set_mode");
 
+    int points_num = 0;
+    nh.param("point_num", points_num, -1);
+    std::vector<std::vector<double>> points(points_num, std::vector<double>(3));
+    for (int i = 0; i < points_num; i++)
+    {
+        nh.param("point" + std::to_string(i) + "_x", points[i][0], -1.0);
+        nh.param("point" + std::to_string(i) + "_y", points[i][1], -1.0);
+        nh.param("point" + std::to_string(i) + "_z", points[i][2], -1.0);
+    }
+
     //the set-point publishing rate MUST be faster than 2Hz
     ros::Rate rate(20.0);
 
@@ -51,17 +66,19 @@ int main(int argc, char **argv)
     while(ros::ok() && !current_state.connected){
         ROS_INFO("fcu connecting...");
         // mind the fcu_url in launch file and gazebo_setup.bash in ~/.bashrc
+        // for Pixhawk 4 or 6c firmware, mind the fcu_url in mavros px4.launch
         ros::spinOnce();
         rate.sleep();
     }
     ROS_INFO("fcu connected!");
 
     geometry_msgs::PoseStamped pose;
-    pose.pose.position.x = 0;
-    pose.pose.position.y = 0;
-    pose.pose.position.z = 1;
+    pose.pose.position.x = points[0][0];
+    pose.pose.position.y = points[0][1];
+    pose.pose.position.z = points[0][2];
 
     //send a few set points before starting
+    ROS_INFO("publishing init points");
     for(int i = 200; ros::ok() && i > 0; --i){
         local_pos_pub.publish(pose);
         ros::spinOnce();
@@ -79,8 +96,10 @@ int main(int argc, char **argv)
     // Necessary for real test, enabling to switch to manual control
     bool if_offboard = false;
     bool if_armed = false;
+    int iter = 0;
 
     while(ros::ok()){
+        // give the highest priority to manual control (RC controller)
         if( current_state.mode != "OFFBOARD" && !if_offboard &&
             (ros::Time::now() - last_request > ros::Duration(5.0))){
             if( set_mode_client.call(offb_set_mode) &&
@@ -101,52 +120,22 @@ int main(int argc, char **argv)
             }
         }
 
-        local_pos_pub.publish(pose);
+        if (reached(current_pose, pose)){
+            ROS_INFO("reached way point %s", std::to_string(iter).c_str());
+            iter += 1;
+            if (iter >= points_num) break;
 
-        ros::spinOnce();
-
-        double dist = sqrt(
-                pow(current_pose.pose.position.x - pose.pose.position.x, 2) +
-                pow(current_pose.pose.position.y - pose.pose.position.y, 2) +
-                pow(current_pose.pose.position.z - pose.pose.position.z, 2));
-        if (dist <= 0.2){
-            ROS_INFO("Takeoff Successfully!");
-            break;
+            pose.pose.position.x = points[iter][0];
+            pose.pose.position.y = points[iter][1];
+            pose.pose.position.z = points[iter][2];
+            for(int i = 100; ros::ok() && i > 0; --i){
+                local_pos_pub.publish(pose);
+                ros::spinOnce();
+                rate.sleep();
+            }
         }
-        rate.sleep();
-    }
-
-    // hover
-    ROS_INFO("Hovering...");
-    for(int i = 200; ros::ok() && i > 0; --i){
         local_pos_pub.publish(pose);
-        ros::spinOnce();
-        rate.sleep();
-    }
 
-    // waypointh
-    ROS_INFO("Moving to target position...");
-    pose.pose.position.x = 0.5;
-    pose.pose.position.y = 0;
-    pose.pose.position.z = 1;
-    while (ros::ok()){
-        local_pos_pub.publish(pose);
-        ros::spinOnce();
-        double dist = sqrt(
-                pow(current_pose.pose.position.x - pose.pose.position.x, 2) +
-                pow(current_pose.pose.position.y - pose.pose.position.y, 2) +
-                pow(current_pose.pose.position.z - pose.pose.position.z, 2));
-        if (dist <= 0.2){
-            ROS_INFO("Reached target position!");
-            break;
-        }
-        rate.sleep();
-    }
-
-    // hover
-    ROS_INFO("Hovering...");
-    for(int i = 200; ros::ok() && i > 0; --i){
-        local_pos_pub.publish(pose);
         ros::spinOnce();
         rate.sleep();
     }
